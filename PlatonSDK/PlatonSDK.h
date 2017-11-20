d68cb412e98d74d732471a7df185935951d70a8d
Bodia Kovbas <you@example.com>
2017-11-20 09:43:49 +0200

    Initial Commit

---
+++ PlatonSDK/PlatonSDK.h
@@ -0,0 +1,19 @@
+//
+//  PlatonSDK.h
+//  PlatonSDK
+//
+//  Created by Bodia Kovbas on 11/20/17.
+//  Copyright © 2017 Devlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlatonSDK.
+FOUNDATION_EXPORT double PlatonSDKVersionNumber;
+
+//! Project version string for PlatonSDK.
+FOUNDATION_EXPORT const unsigned char PlatonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatonSDK/PublicHeader.h>
+
+
